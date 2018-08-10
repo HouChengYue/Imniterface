f9b221ba63cbe64a70ffed096216345b78344438
侯程月 <437286343@qq.com>
2018-08-10 16:12:08 +0800

    代码后半部分

---
+++ app/src/main/java/com/youshiyun/quantongyun/imniterface/Interface/FunctionExcption.java
@@ -0,0 +1,13 @@
+package com.youshiyun.quantongyun.imniterface.Interface;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by 侯程月  2018年08月10日.
+ * QQ 437286343
+ */
+public class FunctionExcption extends NoSuchElementException {
+    public FunctionExcption(String s) {
+        super(s);
+    }
+}
--- app/src/main/java/com/youshiyun/quantongyun/imniterface/Interface/FunctionsManiager.java
+++ app/src/main/java/com/youshiyun/quantongyun/imniterface/Interface/FunctionsManiager.java
@@ -16,43 +16,109 @@ public class FunctionsManiager {
     }
 
     private FunctionsManiager() {
-        mStringNPNRFunctionHashMap=new HashMap<>();
-        mStringNPRFunctionHashMap=new HashMap<>();
-        mStringPNRFunctionHashMap=new HashMap<>();
-        mStringPRFunctionHashMap=new HashMap<>();
+        mStringNPNRFunctionHashMap = new HashMap<>();
+        mStringNPRFunctionHashMap = new HashMap<>();
+        mStringPNRFunctionHashMap = new HashMap<>();
+        mStringPRFunctionHashMap = new HashMap<>();
     }
 
 
-    private HashMap<String,NPNRFunction> mStringNPNRFunctionHashMap;
-    private HashMap<String,NPRFunction> mStringNPRFunctionHashMap;
-    private HashMap<String,PNRFunction> mStringPNRFunctionHashMap;
-    private HashMap<String,PRFunction> mStringPRFunctionHashMap;
+    private HashMap<String, NPNRFunction> mStringNPNRFunctionHashMap;
+    private HashMap<String, NPRFunction> mStringNPRFunctionHashMap;
+    private HashMap<String, PNRFunction> mStringPNRFunctionHashMap;
+    private HashMap<String, PRFunction> mStringPRFunctionHashMap;
 
-    public FunctionsManiager addFunction(NPNRFunction pFunction){
-        mStringNPNRFunctionHashMap.put(pFunction.mFactionName,pFunction);
+    public FunctionsManiager addFunction(NPNRFunction pFunction) {
+        mStringNPNRFunctionHashMap.put(pFunction.mFactionName, pFunction);
         return this;
     }
 
-    public FunctionsManiager addFunction(NPRFunction pFunction){
-        mStringNPRFunctionHashMap.put(pFunction.mFactionName,pFunction);
+    public FunctionsManiager addFunction(NPRFunction pFunction) {
+        mStringNPRFunctionHashMap.put(pFunction.mFactionName, pFunction);
         return this;
     }
 
-    public FunctionsManiager addFunction(PNRFunction pFunction){
-        mStringPNRFunctionHashMap.put(pFunction.mFactionName,pFunction);
-        return this;
-    }
-    public FunctionsManiager addFunction(PRFunction pFunction){
-        mStringPRFunctionHashMap.put(pFunction.mFactionName,pFunction);
+    public FunctionsManiager addFunction(PNRFunction pFunction) {
+        mStringPNRFunctionHashMap.put(pFunction.mFactionName, pFunction);
         return this;
     }
 
-    public  void invokeFuction(String factionName){
-        if (TextUtils.isEmpty(factionName)){
+    public FunctionsManiager addFunction(PRFunction pFunction) {
+        mStringPRFunctionHashMap.put(pFunction.mFactionName, pFunction);
+        return this;
+    }
+
+    public void invokeFuction(String factionName) {
+        if (TextUtils.isEmpty(factionName)) {
             return;
         }
-
+        if (mStringNPNRFunctionHashMap != null) {
+            NPNRFunction lNPNRFunction = mStringNPNRFunctionHashMap.get(factionName);
+            if (lNPNRFunction != null) {
+                lNPNRFunction.function();
+            } else {
+                throw new FunctionExcption("没有该Fuction");
+            }
+        } else {
+            throw new FunctionExcption("没有初始化FunctionsManiager");
+        }
     }
 
+    public <Res> Res invokeFuction(String factionName, Class<Res> c) {
+        if (TextUtils.isEmpty(factionName)) {
+            return null;
+        }
+        if (mStringNPRFunctionHashMap != null) {
+            NPRFunction lNPRFunction = mStringNPRFunctionHashMap.get(factionName);
+            if (lNPRFunction != null) {
+                if (c != null) {
+                    return c.cast(lNPRFunction.function());
+                } else {
+                    return (Res) lNPRFunction.function();
+                }
+            } else {
+                throw new FunctionExcption("没有该Fuction");
+            }
 
+        } else {
+            throw new FunctionExcption("没有初始化FunctionsManiager");
+        }
+    }
+
+    public <Param> void invokeFuction(String factionName, Param data) {
+        if (TextUtils.isEmpty(factionName)) {
+            return;
+        }
+        if (mStringPNRFunctionHashMap != null) {
+            PNRFunction lPNRFunction = mStringPNRFunctionHashMap.get(factionName);
+            if (lPNRFunction != null) {
+                lPNRFunction.function(data);
+            } else {
+                throw new FunctionExcption("没有该Fuction");
+            }
+
+        } else {
+            throw new FunctionExcption("没有初始化FunctionsManiager");
+        }
+    }
+
+    public <Param, Res> Res invokeFuction(String factionName, Param data, Class<Res> c) {
+        if (TextUtils.isEmpty(factionName)) {
+            return null;
+        }
+        if (mStringPRFunctionHashMap != null) {
+            PRFunction lPRFunction = mStringPRFunctionHashMap.get(factionName);
+            if (lPRFunction != null) {
+                if (c != null) {
+                 return c.cast(lPRFunction.function(data));
+                }
+                return (Res) lPRFunction.function(data);
+            } else {
+                throw new FunctionExcption("没有该Fuction");
+            }
+
+        } else {
+            throw new FunctionExcption("没有初始化FunctionsManiager");
+        }
+    }
 }
